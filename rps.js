178dcdbd6b190d653a5ffc53ad527a88bb49a375
Tim <smajda85@gmail.com>
2022-07-25 13:42:32 -0400

    Add pseudocode of single round game to js file

---
+++ rps.js
@@ -0,0 +1,18 @@
+//single round rock paper scissor
+
+//set variable that randomly chooses rock, paper, scissor for a.i.
+
+//have an input for player to type in their choice and store as playerChoice variable
+
+//convert playerChoice string to lowercase
+
+/*if computerChoice is rock code
+    if playerChoice is rock
+        display message "Tie."
+    
+    if playerChoice is paper
+        display message "You won."
+
+    if playerChoice is scissors
+        display message "You lost."
+*/
